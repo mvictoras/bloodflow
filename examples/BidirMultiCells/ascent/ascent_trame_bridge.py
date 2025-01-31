67ffd3c575ad7caac918e90e8217ae782c78570c
TJFord <tan.jifu@gmail.com>
2025-01-31 12:01:56 -0500

    add ascent_trame_bridge.py

--- examples/BidirMultiCells/ascent/ascent_trame_bridge.py
+++ examples/BidirMultiCells/ascent/ascent_trame_bridge.py
@@ -1,20 +1,10 @@
-from hmac import new
-import os
-import sys
-import zipfile
+import conduit
+import ascent.mpi
 
+import numpy as np
 from multiprocessing.managers import BaseManager
 
-import ascent.mpi
-import conduit
-import numpy as np
-import yaml
-
 from mpi4py import MPI
-
-sys.path.append(
-    f"../../.venv/lib/python{sys.version_info.major}.{sys.version_info.minor}/site-packages"
-)
 
 
 class QueueManager(BaseManager):
@@ -27,6 +17,141 @@ def main():
 
     # get task id and number of total tasks
     task_id = comm.Get_rank()
+    num_tasks = comm.Get_size()
+
+    # run Trame tasks
+    interactive = np.array([False], bool)
+    update_data = None
+    if task_id == 0:
+        update_data = executeMainTask(task_id, num_tasks, comm)
+    else:
+        executeDependentTask(task_id, num_tasks, comm)
+    
+    # broadcast updates to all ranks
+    update_data = comm.bcast(update_data, root=0)
+
+    # TODO: pass updates to Ascent callback
+    #update_node = conduit.Node()
+    #update_node['task_id'] = task_id
+    #output = conduit.Node()
+    #ascent.mpi.execute_callback('steeringCallback', update_node, output)
+
+
+def executeMainTask(task_id, num_tasks, comm):
+    interactive = np.array([False], bool)
+    update_data = {}    
+
+    # attempt to connect to Trame queue manager
+    QueueManager.register('get_data_queue')
+    QueueManager.register('get_signal_queue')
+    mgr = QueueManager(address=('127.0.0.1', 8000), authkey=b'ascent-trame')
+    try:
+        mgr.connect()
+        interactive[0] = True
+    except:
+        mgr = None
+
+    # broadcast to all processes whether Trame is currently running
+    comm.Bcast((interactive, 1, MPI.BOOL), root=0)
+
+    if interactive[0]:
+        # get access to Trame's queues
+        queue_data = mgr.get_data_queue()
+        queue_signal = mgr.get_signal_queue()
+
+        # get published blueprint data
+        mesh_data = ascent_data().child(0)
+
+        # TODO: repartition data -> gather on main process (0)
+        #result = repartitionMeshData(task_id, num_tasks, comm)
+
+        # extract red blood cell mesh polydata
+        x_coords = mesh_data['coordsets/particle_coords/values/x']
+        y_coords = mesh_data['coordsets/particle_coords/values/y']
+        z_coords = mesh_data['coordsets/particle_coords/values/z']
+        vertices = np.column_stack([x_coords, y_coords, z_coords])
+
+        faces = np.reshape(mesh_data['topologies/particle_topo/elements/connectivity'], (-1, 3))
+
+        # send rbc data to Trame
+        queue_data.put({'vertices': vertices, 'faces': faces})
+        
+        # get steering updates from Trame
+        update_data = queue_signal.get()
+
+    return update_data
+
+
+def executeDependentTask(task_id, num_tasks, comm):
+    interactive = np.array([False], bool)
+
+    # receive whether session is interactive or not from main task
+    comm.Bcast((interactive, 1, MPI.BOOL), root=0)
+
+    if interactive[0]:
+        # TODO: repartition data -> gather on main process (0)
+        #repartitionMeshData(task_id, num_tasks, comm)
+        pass
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+import sys
+
+from hmac import new
+import os
+import zipfile
+
+from multiprocessing.managers import BaseManager
+
+import ascent.mpi
+import conduit
+import numpy as np
+import yaml
+
+from vtkmodules.vtkCommonCore import vtkPoints
+from vtkmodules.vtkCommonDataModel import vtkCellArray, vtkPolyData
+from vtkmodules.vtkIOXML import vtkXMLPolyDataWriter
+
+from mpi4py import MPI
+
+
+class QueueManager(BaseManager):
+    pass
+
+
+def main():
+    # obtain a mpi4py mpi comm object
+    comm = MPI.Comm.f2py(ascent_mpi_comm_id())
+
+    # get task id and number of total tasks
+    task_id = comm.Get_rank()
+    num_tasks = comm.Get_size()
+
+    ###
+    # TEST
+    ###
+    # get published blueprint data
+    mesh_data = ascent_data().child(0)    
+
+    x_coords = mesh_data['coordsets/particle_coords/values/x']
+    y_coords = mesh_data['coordsets/particle_coords/values/y']
+    z_coords = mesh_data['coordsets/particle_coords/values/z']
+    vertices = np.column_stack([x_coords, y_coords, z_coords])
+
+    faces = np.reshape(mesh_data['topologies/particle_topo/elements/connectivity'], (-1, 3))
+
+    poly_data = createTriangleVtkPolyData(vertices, faces)
+
+    writer = vtkXMLPolyDataWriter()
+    writer.SetFileName('ascent_rbc.vtp')
+    writer.SetInputData(poly_data)
+    writer.Write()
+    ###
+    # END: TEST
+    ###
 
     # run Trame tasks
     interactive = np.array([False], bool)
@@ -108,5 +233,26 @@ def executeDependentTask(comm):
         pass
 
 
+def createTriangleVtkPolyData(vertices, faces):
+    points = vtkPoints()
+    for v in vertices:
+        points.InsertNextPoint(v)
+
+    triangles = vtkCellArray()
+    for f in faces:
+        triangles.InsertNextCell(3)
+        for i in f:
+            triangles.InsertCellPoint(i)
+
+    poly_data = vtkPolyData()
+    poly_data.SetPoints(points)
+    poly_data.SetPolys(triangles)
+
+    return poly_data
+
+
 if __name__ == "__main__":
     main()
+
+"""
+
